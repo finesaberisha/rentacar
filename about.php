b3b26cb431dd4198a2873964b58f7f80879180a2
fionaveseli <74466339+fionaveseli@users.noreply.github.com>
2023-05-04 20:21:28 +0200

    header

--- about.php
+++ about.php
@@ -33,7 +33,12 @@ require 'header.php';
             </li>
             <li>
             <h3>Rent a Kerr Company</h3>
-          <p>If you're planning a trip to Kosovo and need a reliable and affordable way to get around, look no further than our rent-a-car company. Based in the heart of Prishtina, we offer a wide range of vehicles to suit every need and budget, from compact cars to spacious SUVs and everything in between. But we don't just serve Prishtina - we also have branches in Gjakova, Peja, Prizren, Ferizaj, and Mitrovica, making it easy to rent a car no matter where you're headed. <button class="btn">Login</button></p>
+            <br>
+          <p>If you're planning a trip to Kosovo and need a reliable and affordable way to get around, look no further than our rent-a-car company. Based in the heart of Prishtina, we offer a wide range of vehicles to suit every need and budget, from compact cars to spacious SUVs and everything in between. But we don't just serve Prishtina - we also have branches in Gjakova, Peja, Prizren, Ferizaj, and Mitrovica, making it easy to rent a car no matter where you're headed.</p>
+          <br>
+          <a href="register.php">
+            <button class="btn">Login</button>
+          </a>
             </li>
         </div>
       </section>
